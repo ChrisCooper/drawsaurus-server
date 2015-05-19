17f77bf914c5806eb59f8d3db02798f28a1ebcb4
Chris Cooper <chris@quixotic-chris.com>
2015-05-18 22:45:40 -0400

    removed problematic relation

--- drawsaurus/models.py
+++ drawsaurus/models.py
@@ -13,7 +13,7 @@ class Game(models.Model):
 class Turn(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     turn_number = models.IntegerField()
-    author = models.ForeignKey(User, related_name='turns')
+    author = models.ForeignKey(User)
     game = models.ForeignKey(Game)
 
     class Meta:
